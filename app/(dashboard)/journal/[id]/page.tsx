12bbe4c48dfaee4a66acabe7781d27ac62099769
EamonnHegarty <eamonnhegartyy@hotmail.co.uk>
2025-03-18 16:01:20 +0000

    added journal side grid

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -31,9 +31,48 @@ const EntryPage = async ({ params }: { params: Params }) => {
   const { id } = await params;
   const entry = await getEntry(id);
 
+  const analysisData = [
+    {
+      name: "Summary",
+      value: "",
+    },
+    {
+      name: "Subject",
+      value: "",
+    },
+    {
+      name: "Mood",
+      value: "",
+    },
+    {
+      name: "Negative",
+      value: "False",
+    },
+  ];
+
   return (
-    <div className="h-full w-full">
-      <Editor entry={entry as Entry} />
+    <div className="grid h-full w-full grid-cols-3">
+      <div className="col-span-2">
+        <Editor entry={entry as Entry} />
+      </div>
+      <div className="border-l border-black/10">
+        <div className="bg-blue-300 px-6 py-10">
+          <h2 className="text-2xl">Analysis</h2>
+        </div>
+        <div>
+          <ul>
+            {analysisData.map((item) => (
+              <li
+                key={item.name}
+                className="flex items-center justify-between border-t border-b border-black/10 px-2 py-4"
+              >
+                <span className="text-lg font-semibold">{item.name}</span>
+                <span>{item.value}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
