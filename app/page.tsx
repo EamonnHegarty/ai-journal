d45767379d8709fedfa29cbd7574f129b131f49d
EamonnHegarty <eamonnhegartyy@hotmail.co.uk>
2025-03-21 10:18:49 +0000

    spelling

--- app/page.tsx
+++ app/page.tsx
@@ -7,16 +7,16 @@ export default async function Home() {
   const href = userId ? "/journal" : "/new-user";
 
   return (
-    <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
-      <div className="w-full max-w-[600px] mx-auto">
-        <h1 className="text-6xl mb-4">AI Journal</h1>
-        <p className="text-2xl text-white/60 mb-4">
+    <div className="flex h-screen w-screen items-center justify-center bg-black text-white">
+      <div className="mx-auto w-full max-w-[600px]">
+        <h1 className="mb-4 text-6xl">AI Journal</h1>
+        <p className="mb-4 text-2xl text-white/60">
           The best app for tracking your mood through life. All we ask is that
-          you be honest
+          you be honest.
         </p>
         <div>
           <Link href={href}>
-            <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl cursor-pointer">
+            <button className="cursor-pointer rounded-lg bg-blue-600 px-4 py-2 text-xl">
               Get started
             </button>
           </Link>
